37998b64dd5c00097491b5fbc746fddd04559fac
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-03-20 16:04:41 +0200

    Work on week 2 ohte

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -6,4 +6,34 @@ class TestMaksukortti(unittest.TestCase):
         self.maksukortti = Maksukortti(1000)
 
     def test_luotu_kortti_on_olemassa(self):
-        self.assertNotEqual(self.maksukortti, None)
+        kortti = self.maksukortti
+        self.assertNotEqual(str(kortti), None)
+    
+    def test_saldo_alussa_oikein(self):
+        kortti = self.maksukortti
+        self.assertEqual(str(kortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        kortti = self.maksukortti
+        kortti.lataa_rahaa(200)
+        self.assertEqual(str(kortti), "Kortilla on rahaa 12.00 euroa")
+
+    def test_saldo_vähenee_jos_rahaa(self):
+        kortti = self.maksukortti
+        kortti.ota_rahaa(500)
+        self.assertEqual(str(kortti), "Kortilla on rahaa 5.00 euroa")
+
+    def test_saldo_ei_vähene_jos_ei_rahaa(self):
+        kortti = self.maksukortti
+        kortti.ota_rahaa(1500)
+        self.assertEqual(str(kortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_metodi_palauttaa_false(self):
+        x = self.maksukortti.ota_rahaa(1500)
+        self.assertFalse(x)
+    
+    def test_metodi_palauttaa_true(self):
+        x=self.maksukortti.ota_rahaa(900)
+        self.assertTrue(x)
+    
+    
